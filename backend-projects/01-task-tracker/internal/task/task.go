90a0c417d391ccfe0e090c7fd3d03ee52687e2d2
DimaChekashov <foxdima99@gmail.com>
2025-03-24 22:02:44 +0300

    implements read and write file fs

--- backend-projects/01-task-tracker/internal/task/task.go
+++ backend-projects/01-task-tracker/internal/task/task.go
@@ -1,1 +1,24 @@
 package task
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+type TaskStatus string
+
+const (
+	TASK_STATUS_TODO        TaskStatus = "todo"
+	TASK_STATUS_IN_PROGRESS TaskStatus = "in-progress"
+	TASK_STATUS_DONE        TaskStatus = "done"
+)
+
+type Task struct {
+	ID          int64      `json:"id"`
+	Description string     `json:"description"`
+	Status      TaskStatus `json:"status"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
+}
