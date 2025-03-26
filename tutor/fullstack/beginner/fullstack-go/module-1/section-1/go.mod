eb0263d334a0cb283d992749b456314859dfa6d6
DimaChekashov <foxdima99@gmail.com>
2025-03-26 11:09:25 +0300

    remove tutor folder

--- tutor/fullstack/beginner/fullstack-go/module-1/section-1/go.mod
+++
@@ -1,5 +0,0 @@
-module example.com/hello
-
-go 1.20
-
-require github.com/google/uuid v1.6.0 // indirect
--- tutor/fullstack/beginner/fullstack-nodejs/module-1/app-5/db.js
+++
@@ -1,3 +0,0 @@
-export const connect = () => console.log("Connected to DB");
-
-export const query = (sql) => console.log(`${sql} queried`);
