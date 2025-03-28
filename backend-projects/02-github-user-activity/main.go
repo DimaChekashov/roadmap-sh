26e7597b02878e2be909f5d0d41331ac0230c7f0
DimaChekashov <foxdima99@gmail.com>
2025-03-28 23:33:06 +0300

    init cmd in main func

--- backend-projects/02-github-user-activity/main.go
+++ backend-projects/02-github-user-activity/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/DimaChekashov/roadmap-sh/backend-projects/02-github-user-activity/cmd"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+
+	if err := rootCmd.Execute(); err != nil {
+		errorStyle := lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FF0000")).
+			Padding(1, 2).
+			Bold(true).
+			Render(fmt.Sprintf("Error: %s", err))
+
+		fmt.Println(errorStyle)
+	}
 }
