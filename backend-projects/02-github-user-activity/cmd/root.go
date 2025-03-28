4adac1ed3a0b9ac31eaf720a24db555c884ea199
DimaChekashov <foxdima99@gmail.com>
2025-03-28 23:29:24 +0300

    implements cmd command

--- backend-projects/02-github-user-activity/cmd/root.go
+++ backend-projects/02-github-user-activity/cmd/root.go
@@ -1,1 +1,38 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/DimaChekashov/roadmap-sh/backend-projects/02-github-user-activity/internal/activity"
+	"github.com/spf13/cobra"
+)
+
+func NewRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "github-activity",
+		Short: "Github User Activity is a CLI tool for fetching user activity",
+		Long: `Github User Activity is a CLI tool for fetching user activity. It allows you to fetch user activity by providing the username.
+
+Example:
+> github-activity DimaChekashov`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunDisplayActivityCmd(args)
+		},
+	}
+
+	return cmd
+}
+
+func RunDisplayActivityCmd(args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("please provide a username")
+	}
+
+	username := args[0]
+	activities, err := activity.FetchGitHubActivity(username)
+	if err != nil {
+		return err
+	}
+
+	return activity.DisplayActivity(username, activities)
+}
