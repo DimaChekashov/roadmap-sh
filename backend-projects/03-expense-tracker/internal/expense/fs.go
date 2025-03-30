5cd1220fdf56682ee86c9c490e443baa4945e178
DimaChekashov <foxdima99@gmail.com>
2025-03-30 23:56:34 +0300

    implements get file paths

--- backend-projects/03-expense-tracker/internal/expense/fs.go
+++ backend-projects/03-expense-tracker/internal/expense/fs.go
@@ -1,1 +1,28 @@
 package expense
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+)
+
+func expensesFilePath() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return ""
+	}
+
+	return path.Join(cwd, "expenses.json")
+}
+
+func budgetsFilePath() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return ""
+	}
+
+	return path.Join(cwd, "budgets.json")
+}
