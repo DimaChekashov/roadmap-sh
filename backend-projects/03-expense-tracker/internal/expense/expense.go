ae78b9ca68b167c098ad82a7ba7b491405f2ef74
DimaChekashov <foxdima99@gmail.com>
2025-04-06 23:13:36 +0300

    complete expenses

--- backend-projects/03-expense-tracker/internal/expense/expense.go
+++ backend-projects/03-expense-tracker/internal/expense/expense.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/03-expense-tracker/internal/expense"
 	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/03-expense-tracker/internal/log"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -63,5 +64,103 @@ func AddExpense(description string, amount float64, category string) error {
 	}
 
 	log.Info(fmt.Sprintf("Expense added: %s, Amount: %.2f, Category: %s (ID: %d)", description, amount, category, newExpenseId))
+	return WriteExpensesToFile(expenses)
+}
+
+func ListExpenses(category string) error {
+	expenses, err := ReadExpensesFromFile()
+	if err != nil {
+		return err
+	}
+
+	if len(expenses) == 0 {
+		log.Error("No expenses found")
+		return nil
+	}
+
+	var filteredExpenses []Expense
+	for _, exponse := range expenses {
+		if category == "all" || strings.EqualFold(expense.Category, category) {
+			filteredExpenses = append(filteredExpenses, exponse)
+		}
+	}
+
+	if len(filteredExpenses) == 0 {
+		log.Info(fmt.Sprintf("No expenses found for category: %s", category))
+		return nil
+	}
+
+	fmt.Println(
+		lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#FFCC66")).
+			Margin(1, 0).
+			Render(fmt.Sprintf("Expenses (%s)", category)),
+	)
+	for _, expense := range filteredExpenses {
+		formattedId := lipgloss.NewStyle().
+			Bold(true).
+			Width(8).
+			Render(fmt.Sprintf("ID: %d", expense.ID))
+
+		expenseStyle := lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder(), false, false, true, false).
+			BorderForeground(lipgloss.Color("#3C3C3C")).
+			Render(fmt.Sprintf("%s %s %.2f %s", formattedId, expense.Description, expense.Amount, expense.Category))
+		fmt.Println(expenseStyle)
+	}
+
+	return nil
+}
+
+func SummaryExpenses(month int) error {
+	expenses, err := ReadExpensesFromFile()
+	if err != nil {
+		return err
+	}
+
+	if len(expenses) == 0 {
+		log.Error("No expenses found")
+		return nil
+	}
+
+	var total float64
+	if month == 0 {
+		for _, expense := range expenses {
+			total += expense.Amount
+		}
+	} else {
+		for _, expense := range expenses {
+			if expense.CreatedAt.Month() == time.Month(month) {
+				total += expense.Amount
+			}
+		}
+	}
+
+	log.Info(fmt.Sprintf("Total expenses: %.2f", total))
+	return nil
+}
+
+func DeleteExpense(id int64) error {
+	expenses, err := ReadExpensesFromFile()
+	if err != nil {
+		return err
+	}
+
+	var found bool
+	for i, expense := range expenses {
+		if expense.ID == id {
+			found = true
+			expenses = append(expenses[:i], expenses[i+1:]...)
+			log.Info(fmt.Sprintf("Expense deleted: %s, Amount: %.2f, Category: %s (ID: %d)", expense.Description, expense.Amount, expense.Category, id))
+			break
+		}
+	}
+
+	if !found {
+		log.Error(fmt.Sprintf("Expense not found with ID: %d", id))
+		return nil
+	}
+
 	return WriteExpensesToFile(expenses)
 }
