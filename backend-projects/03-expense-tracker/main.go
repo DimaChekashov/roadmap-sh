3b62d7efa70f72810daa3dede3784352fcfbfb2d
DimaChekashov <foxdima99@gmail.com>
2025-04-07 23:03:34 +0300

    execute root cmd

--- backend-projects/03-expense-tracker/main.go
+++ backend-projects/03-expense-tracker/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/03-expense-tracker/cmd"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+
+	if err := rootCmd.Execute(); err != nil {
+		errorStyle := lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FF0000")).
+			Padding(1, 2).
+			Bold(true).
+			Render(fmt.Sprintf("Error: %s", err))
+
+		fmt.Println(errorStyle)
+	}
+}
