1fe04a164936c374f5792b3da4ffd5c6accdc977
DimaChekashov <foxdima99@gmail.com>
2025-04-07 22:52:07 +0300

    implements list command

--- backend-projects/03-expense-tracker/cmd/list.go
+++ backend-projects/03-expense-tracker/cmd/list.go
@@ -1,1 +1,23 @@
 package cmd
+
+import (
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/03-expense-tracker/internal/expense"
+	"github.com/spf13/cobra"
+)
+
+func NewListCmd() *cobra.Command {
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all expenses",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunListExpenseCmd(args)
+		},
+	}
+
+	listCmd.Flags().StringVarP(&Category, "category", "c", "all", "Filter expenses by category")
+	return listCmd
+}
+
+func RunListExpenseCmd(args []string) error {
+	return expense.ListExpenses(Category)
+}
