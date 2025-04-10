5daf90fbd68bc5817b404d0b79575f9b04562269
DimaChekashov <foxdima99@gmail.com>
2025-04-10 22:45:32 +0300

    implements router

--- backend-projects/05-unit-converter/router/router.go
+++ backend-projects/05-unit-converter/router/router.go
@@ -1,1 +1,32 @@
 package router
+
+import (
+	"log/slog"
+
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/05-unit-converter/handler"
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	log     *slog.Logger
+	handler *handler.Handler
+}
+
+func NewRouter(log *slog.Logger, handler *handler.Handler) *Router {
+	return &Router{
+		log:     log,
+		handler: handler,
+	}
+}
+
+func (r *Router) initRouter() *gin.Engine {
+	g := gin.Default()
+
+	g.POST("/convert", r.handler.Convert)
+
+	return g
+}
+
+func (r *Router) StartServer() {
+	r.initRouter().Run(":8080")
+}
