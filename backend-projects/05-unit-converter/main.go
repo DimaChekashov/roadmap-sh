e05842f6c74145d483cd4011097d9e2d35b60403
DimaChekashov <foxdima99@gmail.com>
2025-04-11 23:24:45 +0300

    complete unit converter

--- backend-projects/05-unit-converter/main.go
+++ backend-projects/05-unit-converter/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log/slog"
+
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/05-unit-converter/handler"
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/05-unit-converter/router"
+	"github.com/DimaChekashov/roadmap-sh/tree/main/backend-projects/05-unit-converter/service"
+)
+
+func main() {
+	log := slog.Default()
+
+	service := service.NewService(log)
+
+	handler := handler.NewHandler(log, service)
+
+	router := router.NewRouter(log, handler)
+
+	router.StartServer()
+}
