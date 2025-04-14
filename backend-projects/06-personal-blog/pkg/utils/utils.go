6b02407b39e1c4312ed6b0fd5438a2645a66db0a
DimaChekashov <foxdima99@gmail.com>
2025-04-14 23:09:01 +0300

    add GetID

--- backend-projects/06-personal-blog/pkg/utils/utils.go
+++ backend-projects/06-personal-blog/pkg/utils/utils.go
@@ -1,5 +1,14 @@
 package utils
 
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
 var currentId int
 
 func MethodResponse(w http.ResponseWriter, r *http.Request, method string) {
@@ -8,4 +17,21 @@ func MethodResponse(w http.ResponseWriter, r *http.Request, method string) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+}
+
+func GetID(w http.ResponseWriter, r *http.Request) int {
+	id := chi.URLParam(r, "id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		WriteJsonError(w, "Invalid ID", http.StatusBadRequest, err)
+		return 0
+	}
+	return idInt
+}
+
+type Response struct {
+	Message string      `json:"message"`
+	Error   string      `json:"error,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	Success bool        `json:"success,omitempty"`
 }
